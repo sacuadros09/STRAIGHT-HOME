d92cb20adb129892a72b57476b678c96b4f08aae
sacuadros09 <sacuma09@gmail.com>
2023-05-04 20:33:15 -0500

    palabras puestas

--- src/screens/login/dashlogin.ts
+++ src/screens/login/dashlogin.ts
@@ -23,22 +23,42 @@ connectedCallback(){
         const rightlogin = this.ownerDocument.createElement("section")
         rightlogin.className= "right"
 
-        
+        const create = this.ownerDocument.createElement("h2")
+        create.innerText = "Iniciar sesión"
+         rightlogin.appendChild(create)
+
+         const account = this.ownerDocument.createElement("h1")
+         account.innerText = "¿No tienes una cuenta? Regístrate"
+          rightlogin.appendChild(account)
+
+          const usuario = this.ownerDocument.createElement("h1")
+         usuario.innerText = "Email"
+          rightlogin.appendChild(usuario)
+          
             const login = this.ownerDocument.createElement("my-loginput");
             login.setAttribute(attr.placeholder,"email");
             login.setAttribute(attr.type,"email" );
             rightlogin.appendChild(login)
+
+            const email = this.ownerDocument.createElement("h1")
+            email.innerText = "Contraseña"
+             rightlogin.appendChild(email)
 
             const password = this.ownerDocument.createElement("my-loginput");
             password.setAttribute(attr.placeholder,"password");
             password.setAttribute(attr.type,"password" );
             rightlogin.appendChild(password)
 
+            const espacio = this.ownerDocument.createElement("h1")
+            espacio.innerText = ""
+             rightlogin.appendChild(espacio)
+
+
             const button = this.ownerDocument.createElement("my-butlogin");
             button.innerText= "Iniciar sesión"
             rightlogin.appendChild(button)
 
-
+           
 
 
 
@@ -49,6 +69,7 @@ connectedCallback(){
 
 
             countainer.appendChild(rightlogin)
+            
         
             this.shadowRoot?.appendChild (countainer)
 
--- src/screens/register/dashregister.ts
+++ src/screens/register/dashregister.ts
@@ -23,21 +23,45 @@ connectedCallback(){
         const rightregister = this.ownerDocument.createElement("section")
         rightregister.className= "right"
 
+        const create = this.ownerDocument.createElement("h2")
+        create.innerText = "Crear una cuenta"
+         rightregister.appendChild(create)
+
+         const have  = this.ownerDocument.createElement("h1")
+         have.innerText = "¿Ya tienes una cuenta? Inicia sesión"
+         rightregister.appendChild(have)
+
+         
+         const usuario = this.ownerDocument.createElement("h1")
+         usuario.innerText = "Usuario"
+          rightregister.appendChild(usuario)
         
             const register = this.ownerDocument.createElement("my-reginput");
             register.setAttribute(att.placeholder,"Usuario");
             register.setAttribute(att.type,"Usuario" );
             rightregister.appendChild(register)
 
+            const correo = this.ownerDocument.createElement("h1")
+            correo.innerText = "Email"
+             rightregister.appendChild(correo)
+
             const email = this.ownerDocument.createElement("my-reginput");
             email.setAttribute(att.placeholder,"Email");
             email.setAttribute(att.type,"Email" );
             rightregister.appendChild(email)
 
+            const pass = this.ownerDocument.createElement("h1")
+            pass.innerText = "Contraseña"
+             rightregister.appendChild(pass)
+
             const password = this.ownerDocument.createElement("my-reginput");
             password.setAttribute(att.placeholder,"Contraseña");
             password.setAttribute(att.type,"Contraseña" );
             rightregister.appendChild(password)
+
+            const espacio = this.ownerDocument.createElement("h1")
+            espacio.innerText = ""
+             rightregister.appendChild(espacio)
 
             const button = this.ownerDocument.createElement("my-butregister");
             button.innerText= "Contraseña"
